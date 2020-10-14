bfbaef30a036d2a28fc361dbb60310e9e5b26d37
cijie xia <cijie@ualberta.ca>
2020-10-14 08:44:12 -0700

    add unit tests for ABsVPValue, AbsOpArray, AbsOpStack.
    Signed-off-by: cijie xia <cijie@ualberta.ca>

--- fvtest/compilerunittest/abstractinterpreter/AbsInterpreterTest.hpp
+++ fvtest/compilerunittest/abstractinterpreter/AbsInterpreterTest.hpp
@@ -19,64 +19,66 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-#ifndef ABS_INTERPRETER_TEST_HPP
-#define ABS_INTERPRETER_TEST_HPP
+#ifndef ABS_INTERPRETER_HPP
+#define ABS_INTERPRETER_HPP
 
 #include <gtest/gtest.h>
 #include <exception>
-#include "../CompilerUnitTest.hpp"
-#include "optimizer/abstractinterpreter/AbsValue.hpp"
+#include "../CodeGenTest.hpp"
+#include "env/SystemSegmentProvider.hpp"
+#include "env/Region.hpp"
+#include "compile/Compilation.hpp"
+#include "compile/Compilation_inlines.hpp"
 #include "optimizer/GlobalValuePropagation.hpp"
+
+
 
 namespace TRTest {
 
-class AbsInterpreterTest : public TRTest::CompilerUnitTest {
-public:
-    AbsInterpreterTest() : CompilerUnitTest() {
-        _manager = new (_comp.allocator()) TR::OptimizationManager(_optimizer, TR::GlobalValuePropagation::create, OMR::globalValuePropagation);
-        _vp = static_cast<TR::GlobalValuePropagation*>(TR::GlobalValuePropagation::create(_manager));
-        _vp->initialize();
-    }
 
-   TR::ValuePropagation* vp() { return _vp; }
+class AbsInterpreterTest : public ::testing::Test
+   {
+   public:
+   AbsInterpreterTest() :
+      _jitInit(),
+      _rawAllocator(),
+      _segmentProvider(1 << 16, _rawAllocator),
+      _dispatchRegion(_segmentProvider, _rawAllocator),
+      _trMemory(*OMR::FrontEnd::singleton().persistentMemory(), _dispatchRegion),
+      _types(),
+      _options(),
+      _ilGenRequest(),
+      _method("compunittest", "0", "test", 0, NULL, _types.NoType, NULL, NULL),
+      _comp(0, NULL, &OMR::FrontEnd::singleton(), &_method, _ilGenRequest, _options, _dispatchRegion, &_trMemory, TR_OptimizationPlan::alloc(warm)),
+      _optimizer(&_comp, NULL, false),
+      _manager(&_optimizer, TR::GlobalValuePropagation::create, OMR::globalValuePropagation),
+      _vp(&_manager)
+      {}
 
-private:
-   TR::OptimizationManager* _manager;
-   TR::GlobalValuePropagation* _vp;
-};
 
-class AbsTestValue : public TR::AbsValue {
-public:
-    AbsTestValue(TR::DataType dataType, int32_t low, int32_t high) :TR::AbsValue(dataType), _low(low), _high(high) {}
+   TR::ValuePropagation* vp() { return &_vp; }
+   TR::Region& region() { return _dispatchRegion; }
 
-    int32_t getLow() const { return _low; }
-    int32_t getHigh() const { return _high; }
+   private:
+   JitInitializer _jitInit;
 
-    virtual TR::AbsValue* clone(TR::Region& region) const {
-        TRTest::AbsTestValue* copy = new (region) TRTest::AbsTestValue(_dataType, _paramPos, _low, _high);
-        return copy;
-    } 
+   TR::RawAllocator _rawAllocator;
+   TR::SystemSegmentProvider _segmentProvider;
+   TR::Region _dispatchRegion;
+   TR_Memory _trMemory;
 
-    virtual bool isTop() const { return _low == INT_MIN && _high == INT_MAX; }
+   TR::TypeDictionary _types;
 
-    virtual void setToTop() { _low = INT_MIN; _high = INT_MAX;  }
+   TR::Options _options;
+   NullIlGenRequest _ilGenRequest;
+   TR::ResolvedMethod _method;
+   TR::Compilation _comp;
 
-    virtual TR::AbsValue* merge(const TR::AbsValue *other) { 
-        const TRTest::AbsTestValue* v = static_cast<const TRTest::AbsTestValue*>(other);
-        _low = std::min(v->getLow(), _low);
-        _high = std::max(v->getHigh(), _high);
-        return this;
-    }
+   TR::Optimizer _optimizer;
+   TR::OptimizationManager _manager;
 
-    virtual void print(TR::Compilation* comp) const {} 
+   TR::GlobalValuePropagation _vp;
 
-private:
-    AbsTestValue(TR::DataType dataType, int32_t paramPos, int32_t low, int32_t high) : TR::AbsValue(dataType, paramPos), _low(low), _high(high) {}
-
-    int32_t _low;
-    int32_t _high;
-};
+   };
 }
-
-
 #endif
