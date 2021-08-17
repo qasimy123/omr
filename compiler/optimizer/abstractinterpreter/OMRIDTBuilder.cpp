c247a8aa674735b75adeac20c3135f8a730ef3cf
dinolii <dino.li@mail.utoronto.ca>
2021-08-16 22:21:44 -0400

    Add setup for propagation

---
+++ compiler/optimizer/abstractinterpreter/OMRIDTBuilder.cpp
@@ -0,0 +1,204 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#include "optimizer/abstractinterpreter/IDTBuilder.hpp"
+#include "il/Block.hpp"
+
+OMR::IDTBuilder::IDTBuilder(TR::ResolvedMethodSymbol* symbol, int32_t budget, TR::Region& region, TR::Compilation* comp, TR_InlinerBase* inliner) :
+        _rootSymbol(symbol),
+        _rootBudget(budget),
+        _region(region),
+        _comp(comp),
+        _inliner(inliner),
+        _idt(NULL)
+{}
+
+TR::IDTBuilder* OMR::IDTBuilder::self()
+{
+    return static_cast<TR::IDTBuilder*>(this);
+}
+
+TR::IDT* OMR::IDTBuilder::buildIDT()
+{
+    bool traceBIIDTGen = self()->comp()->getOption(TR_TraceBIIDTGen);
+
+    if (traceBIIDTGen)
+        traceMsg(self()->comp(), "\n+ IDTBuilder: Start building IDT |\n\n");
+
+    TR_ResolvedMethod* rootMethod = _rootSymbol->getResolvedMethod();
+    TR_ByteCodeInfo bcInfo;
+
+    //This is just a fake callsite to make ECS work.
+    TR_CallSite *rootCallSite = new (self()->region()) TR_CallSite(
+            rootMethod,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            rootMethod->containingClass(),
+            0,
+            0,
+            rootMethod,
+            _rootSymbol,
+            _rootSymbol->getMethodKind() == TR::MethodSymbol::Kinds::Virtual || _rootSymbol->getMethodKind() == TR::MethodSymbol::Kinds::Interface ,
+            _rootSymbol->getMethodKind() == TR::MethodSymbol::Kinds::Interface,
+            bcInfo,
+            self()->comp());
+
+    TR_CallTarget *rootCallTarget = new (self()->region()) TR_CallTarget(
+            rootCallSite,
+            _rootSymbol,
+            rootMethod,
+            NULL,
+            rootMethod->containingClass(),
+            NULL);
+
+    //Initialize IDT
+    _idt = new (self()->region()) TR::IDT(self()->region(), rootCallTarget, _rootSymbol, _rootBudget, self()->comp());
+
+    TR::IDTNode* root = _idt->getRoot();
+
+    //generate the CFG for root call target
+    TR::CFG* cfg = self()->generateControlFlowGraph(rootCallTarget);
+
+    if (!cfg) //Fail to generate a CFG
+        return _idt;
+
+    //add the IDT decendants
+    TR::AbsValue* returnVal = NULL;
+    self()->buildIDT2(root, NULL, &returnVal, -1, _rootBudget, NULL);
+
+    if (traceBIIDTGen)
+        traceMsg(self()->comp(), "\n+ IDTBuilder: Finish building TR::IDT |\n");
+
+    return _idt;
+}
+
+void OMR::IDTBuilder::buildIDT2(TR::IDTNode* node, TR::AbsArguments* arguments, TR::AbsValue** returnValue, int32_t callerIndex, int32_t budget, TR_CallStack* callStack)
+{
+    TR::ResolvedMethodSymbol* symbol = node->getResolvedMethodSymbol();
+    TR_ResolvedMethod* method = node->getResolvedMethod();
+
+    TR_CallStack* nextCallStack = new (self()->region()) TR_CallStack(self()->comp(), symbol, method, callStack, budget, true);
+
+    TR::IDTBuilderVisitor visitor(self(), node, nextCallStack);
+
+    self()->performAbstractInterpretation(node, visitor, arguments, returnValue, callerIndex);
+}
+
+void OMR::IDTBuilder::addNodesToIDT(TR::IDTNode*parent, int32_t callerIndex, TR_CallSite* callSite, float callRatio, TR::AbsArguments* arguments, TR::AbsValue** returnValue, TR_CallStack* callStack)
+{
+    bool traceBIIDTGen = self()->comp()->getOption(TR_TraceBIIDTGen);
+
+    if (callSite == NULL || callSite->_initialCalleeMethod == NULL)
+    {
+        if (traceBIIDTGen)
+            traceMsg(self()->comp(), "Do not have a callsite. Don't add\n");
+        return;
+    }
+
+    if (callRatio * parent->getRootCallRatio() * 100 < 25)
+    {
+        if (traceBIIDTGen)
+            traceMsg(self()->comp(), "Root call ratio < 0.25. Don't add\n");
+        return;
+    }
+
+    callSite->findCallSiteTarget(callStack, self()->getInliner()); //Find all call targets
+
+    // eliminate call targets that are not inlinable according to the policy
+    // thus they won't be added to TR::IDT
+    self()->getInliner()->applyPolicyToTargets(callStack, callSite);
+
+    if (callSite->numTargets() == 0)
+    {
+        if (traceBIIDTGen)
+            traceMsg(self()->comp(), "Do not have a call target. Don't add\n");
+        return;
+    }
+
+    for (int32_t i = 0 ; i < callSite->numTargets(); i++)
+    {
+        TR_CallTarget* callTarget = callSite->getTarget(i);
+
+        int32_t remainingBudget = parent->getBudget() - callTarget->_calleeMethod->maxBytecodeIndex();
+
+        if (remainingBudget < 0 ) // no budget remains
+        {
+            if (traceBIIDTGen)
+                traceMsg(self()->comp(), "No budget left. Don't add\n");
+            continue;
+        }
+
+        bool isRecursiveCall = callStack->isAnywhereOnTheStack(callTarget->_calleeMethod, 1);
+
+        if (isRecursiveCall) //Stop for recursive call
+        {
+            if (traceBIIDTGen)
+                traceMsg(self()->comp(), "Recursive call. Don't add\n");
+            continue;
+        }
+
+        // The actual symbol for the callTarget->_calleeMethod.
+        TR::ResolvedMethodSymbol* calleeMethodSymbol = TR::ResolvedMethodSymbol::create(self()->comp()->trHeapMemory(), callTarget->_calleeMethod, self()->comp());
+
+        // generate the CFG of this call target and set the block frequencies.
+        TR::CFG* cfg = self()->generateControlFlowGraph(callTarget);
+
+        if (!cfg)
+        {
+            if (traceBIIDTGen)
+                traceMsg(self()->comp(), "Fail to generate a CFG. Don't add\n");
+            continue;
+        }
+
+        if (traceBIIDTGen)
+            traceMsg(self()->comp(), "+ IDTBuilder: Adding a child Node: %s for TR::IDTNode: %s\n", calleeMethodSymbol->signature(self()->comp()->trMemory()), parent->getName(self()->comp()->trMemory()));
+
+        TR::IDTNode* child = parent->addChild(
+                _idt->getNextGlobalIDTNodeIndex(),
+                callTarget,
+                calleeMethodSymbol,
+                callSite->_byteCodeIndex,
+                callRatio,
+                _idt->getRegion()
+        );
+
+        _idt->increaseGlobalIDTNodeIndex();
+
+        if (!self()->comp()->incInlineDepth(calleeMethodSymbol, callSite->_bcInfo, callSite->_cpIndex, NULL, !callSite->isIndirectCall(), 0))
+            continue;
+
+        // Build the IDT recursively
+        self()->buildIDT2(child, arguments, returnValue, callerIndex + 1, child->getBudget(), callStack);
+
+        self()->comp()->decInlineDepth(true);
+    }
+}
+
+void TR::IDTBuilderVisitor::visitCallSite(TR_CallSite* callSite, int32_t callerIndex, TR::Block* callBlock, TR::AbsArguments* arguments, TR::AbsValue** returnValue)
+{
+    if (callBlock->getFrequency() < 6 || callBlock->isCold() || callBlock->isSuperCold())
+        return;
+
+    float callRatio = (float)callBlock->getFrequency() / (float)_idtNode->getCallTarget()->_cfg->getStart()->asBlock()->getFrequency();
+    _idtBuilder->addNodesToIDT(_idtNode, callerIndex, callSite, callRatio, arguments, returnValue, _callStack);
+}
---
+++ compiler/optimizer/abstractinterpreter/OMRIDTBuilder.hpp
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#ifndef OMR_IDT_BUILDER_INCL
+#define OMR_IDT_BUILDER_INCL
+
+/*
+ * The following #define and typedef must appear before any #includes in this file
+ */
+#ifndef OMR_IDT_BUILDER_CONNECTOR
+#define OMR_IDT_BUILDER_CONNECTOR
+namespace OMR { class IDTBuilder; }
+namespace OMR { typedef OMR::IDTBuilder IDTBuilderConnector; }
+#endif
+
+#include "il/ResolvedMethodSymbol.hpp"
+#include "env/Region.hpp"
+#include "compile/Compilation.hpp"
+#include "optimizer/Inliner.hpp"
+#include "optimizer/abstractinterpreter/AbsVisitor.hpp"
+#include "optimizer/abstractinterpreter/IDT.hpp"
+#include "optimizer/abstractinterpreter/IDTNode.hpp"
+
+namespace TR { class IDTBuilder; }
+namespace TR { class IDTBuilderVisitor; }
+
+namespace OMR
+{
+
+    class IDTBuilder
+    {
+        friend class ::TR::IDTBuilderVisitor;
+
+    public:
+
+        IDTBuilder(TR::ResolvedMethodSymbol* symbol, int32_t budget, TR::Region& region, TR::Compilation* comp, TR_InlinerBase* inliner);
+
+        /**
+         * @brief building the IDT in DFS order.
+         * It starts from creating the root IDTNode using the _rootSymbol
+         * and then builds the IDT recursively.
+         * It stops when no more call site is found or the budget runs out.
+         *
+         * @return the inlining dependency tree
+         *
+         */
+        TR::IDT* buildIDT();
+
+        TR::IDTBuilder* self();
+
+    protected:
+
+        TR::Compilation* comp() { return _comp; };
+        TR::Region& region() { return _region; };
+        TR_InlinerBase* getInliner()  { return _inliner; };
+
+    private:
+
+        /**
+         * @brief generate the control flow graph of a call target so that the abstract interpretation can use.
+         *
+         * @note: This method needs language specific implementation.
+         *
+         * @param callTarget the call target to generate CFG for
+         * @return the control flow graph
+         */
+        TR::CFG* generateControlFlowGraph(TR_CallTarget* callTarget) { TR_UNIMPLEMENTED(); return NULL; }
+
+        /**
+         * @brief Perform the abstract interpretation on the method in the IDTNode.
+         *
+         * @note: This method needs language specific implementation.
+         *
+         * @param node the node to be abstract interpreted
+         * @param visitor the visitor which defines the callback method
+         *                that will be called when visiting a call site during abtract interpretation.
+         * @param arguments the arguments are the AbsValues passed from the caller method.
+         * @param callerIndex the caller index
+         */
+        void performAbstractInterpretation(TR::IDTNode* node, TR::IDTBuilderVisitor& visitor, TR::AbsArguments* arguments, TR::AbsValue** returnValue, int32_t callerIndex) { TR_UNIMPLEMENTED(); }
+
+        /**
+         * @param node the node to build a sub IDT for
+         * @param arguments the arguments passed from caller method
+         * @param callerIndex the caller index
+         * @param budget the budget for the sub IDT
+         * @param callStack the call stack
+         */
+        void buildIDT2(TR::IDTNode* node, TR::AbsArguments* arguments, TR::AbsValue** returnValue, int32_t callerIndex, int32_t budget, TR_CallStack* callStack);
+
+        /**
+         * @brief add IDTNode(s) to the IDT
+         *
+         * @param parent the parent node to add children for
+         * @param callerIndex the caller index
+         * @param callSite the call site
+         * @param callRatio the call ratio of this callsite
+         * @param arguments the arguments passed from the caller method.
+         * @param callStack the call stack
+         *
+         * @return void
+         */
+        void addNodesToIDT(TR::IDTNode* parent, int32_t callerIndex, TR_CallSite* callSite, float callRatio, TR::AbsArguments* arguments, TR::AbsValue** returnValue, TR_CallStack* callStack);
+
+        TR::IDT* _idt;
+        TR::ResolvedMethodSymbol* _rootSymbol;
+
+        int32_t _rootBudget;
+        TR::Region& _region;
+        TR::Compilation* _comp;
+        TR_InlinerBase* _inliner;
+    };
+}
+
+namespace TR {
+
+    class IDTBuilderVisitor : public TR::AbsVisitor
+    {
+    public:
+        IDTBuilderVisitor(TR::IDTBuilder* idtBuilder, TR::IDTNode* idtNode, TR_CallStack* callStack) :
+                _idtBuilder(idtBuilder),
+                _idtNode(idtNode),
+                _callStack(callStack)
+        {}
+
+        virtual void visitCallSite(TR_CallSite* callSite, int32_t callerIndex, TR::Block* callBlock, TR::AbsArguments* arguments, TR::AbsValue** returnValue);
+
+    private:
+        TR::IDTBuilder* _idtBuilder;
+        TR::IDTNode* _idtNode;
+        TR_CallStack* _callStack;
+    };
+}
+
+#endif
